2371e2865d27a2e5d3a8d173f5ef7bbc6924c124
igor <igorycha.s@yandex.ru>
2026-01-20 23:13:20 +0300

    Версия 2.2: полная адаптация NavigationBar к системным кнопкам навигации, исправлено позиционирование на всех устройствах

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
