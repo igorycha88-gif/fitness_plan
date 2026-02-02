3a843d8c83ab4a85a49632b29b7f0ae50cf825ee
igor <igorycha.s@yandex.ru>
2026-02-02 17:15:46 +0300

    tests(android): upgrade HiltBasicTest to inject Application via Hilt for DI validation

--- app/src/androidTest/java/com/example/fitness_plan/test/HiltBasicTest.kt
+++ app/src/androidTest/java/com/example/fitness_plan/test/HiltBasicTest.kt
@@ -1,17 +1,21 @@
 package com.example.fitness_plan.test
 
-import androidx.test.core.app.ApplicationProvider
+import android.app.Application
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
 
 @HiltAndroidTest
 class HiltBasicTest {
     @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var app: Application
 
     @Before
     fun init() {
@@ -19,8 +23,7 @@ class HiltBasicTest {
     }
 
     @Test
-    fun appContextIsNotNull() {
-        val ctx = ApplicationProvider.getApplicationContext<android.content.Context>()
-        assertNotNull(ctx)
+    fun appIsInjected() {
+        assertNotNull(app)
     }
 }
